b31e61e84affba2e54bf862d309d43fa95f09b52
DM Web Services <dan@dmwebservices.uk>
2025-06-09 08:03:49 +0100

    Fix: Basic UI Styles ( Design )

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,60 @@
-const changeColor = document.getElementById('change-bk-color');
-const changeBk = document.querySelector(".change-color");
-const outPut = document.getElementById("out-put");
-const copyColor = document.getElementById("copy-color");
+const changeColorBtn = document.getElementById('change-color-btn');
+const backgroundEl = document.getElementById("change-color");
+const outputEl = document.getElementById("out-put");
+const copyBtn = document.getElementById("copy-color");
+const historyListEl = document.getElementById("color-history");
 
-let randomColor = "";
+let currentColor = "#3498db";
+const maxHistory = 5;
+const colorHistory = [];
 
-function randomColorGen() {
-    // Generates Random Hex Colour
-    randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0");
-    
-    // Set Random Background Colour 
-    changeBk.style.backgroundColor = randomColor;
- 
-    // Out Put the Colour Value
-    outPut.textContent = `The colour is: ${randomColor}`;
+function applycolor(color) {
+    backgroundEl.style.backgroundColor = color;
+    outputEl.textContent = `Colour is: ${color}`;
+
+    // Update Color History
+    colorHistory.unshift(color);
+    if ( colorHistory.length > maxHistory ) colorHistory.pop(); // Keep Only 5 colors
+
+    updateColorHistoryList();
+}
+
+function updateColorHistoryList() { 
+    historyListEl.innerHTML= "";
+
+    colorHistory.forEach((color) => {
+        const li = document.createElement("li");
+        li.textContent = color;
+        li.style.color = color;
+        li.style.cursor = "pointer";
+        li.title = "Click to copy"
+
+        li.addEventListener("click", () => copyToClipboard(color));
+        historyListEl.appendChild(li);       
+       
+    });
 };
 
-changeColor.addEventListener("click", randomColorGen); 
+function currentColorGen() {
+    // Generates Random Hex Colour
+    currentColor = "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0");
+    
+    applycolor(currentColor);
+};
 
 // Copy Output value
-copyColor.addEventListener("click", () => {
-    navigator.clipboard.writeText(randomColor)
-    .then(() => alert(`${randomColor}`))
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text)
+    .then(() => alert(`${text}`))
     .catch(err => console.error("Error copying", err));
+};
+
+// Event Listeners
+backgroundEl.addEventListener("click", currentColorGen); 
+copyBtn.addEventListener("click", () => {
+    copyToClipboard(currentColor);
 });
+
+// First Render
+applycolor(currentColor);
 
